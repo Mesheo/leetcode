8f971318155191cbde09b6eeefea80b6c8fec91b
Michel Ledig de Carvalho Pereira <m.ledig.mic@gmail.com>
2024-11-04 16:53:02 -0300

    leetcode validanagram

---
+++ 242_valid_anagram.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sorted_s = sorted(s)
+        sorted_t = sorted(t)
+
+        return sorted_s == sorted_t
+    
+
+class Solution2:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = defaultdict(int)
+
+        for letter in s:
+            count[letter] += 1
+        for letter in t:
+            count[letter] -= 1
+
+        for val in count.values():
+            if val != 0:
+                return False
