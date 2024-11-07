936221e7dbbe6ac735d77ddc64dab5ed09801d74
Michel Ledig de Carvalho Pereira <m.ledig.mic@gmail.com>
2024-11-07 11:15:56 -0300

    leetcode day

--- 347_top_k_frequent_elements.py
+++ 347_top_k_frequent_elements.py
@@ -2,23 +2,38 @@ from typing import List
 
 
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        frequency_map = {}
+    def topKFrequent(self, nums: List[int], k=2) -> List[int]:
+        mapa_num_freq = {} 
+        top_k_frequent = [] 
 
-        for num in nums:
-            if num in frequency_map:
-                frequency_map[num] += 1
+        # criando o mapa
+        for num in nums: 
+            if num not in mapa_num_freq:
+                mapa_num_freq[str(num)] = 1
+
             else:
-                frequency_map[num] = 1
+                mapa_num_freq[num] += 1
 
-        most_frequent = 0
-        for number in frequency_map:
-            
-                
-        pass
+        # ordenando as chaves baseado nos valores   
+        for _ in range(k):
+
+            max_freq = -1
+            num_maior_freq = None
+
+            for num, freq in mapa_num_freq.items():
+                if freq > max_freq:
+                    max_freq = freq
+                    num_maior_freq = num    
+
+            top_k_frequent.append(num_maior_freq)
+            del mapa_num_freq[num_maior_freq]
+
+        return top_k_frequent
 
 
+       
+         
 
-nums = [1,1,1,2,2,3]
-solution = Solution()   
+nums = [1, 1, 1, 1, 2, 2, 3]
+solution = Solution()
 print(solution.topKFrequent(nums))
